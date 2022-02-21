8f137699e3935ccb46e92a9ed143d2dbf62c3a22
Artem Dmitrenko <darkrogua@gmail.com>
2022-02-21 20:17:25 +0400

    fix

--- october.js
+++ october.js
@@ -8,13 +8,23 @@ cssFileList.forEach((fileName) => {
    fs.readFile(fileName, function (err, data) {
       if (err) throw err;
       var array = data.toString().split("\n");
+     
       for (i in array) {
-         console.log(array[i]);
+         if (array[i].includes('{%')) {
+            console.log(array[i]);
+            getPartialName(array[i]);
+         } 
       }
    });
 });
 
+function getPartialName(sLine) {
+   let regexp = '/<(.*?)>/g';
+   let matchAll = sLine.matchAll(regexp);
+   console.log(matchAll);
+}
 
+function getPartial() {
 
-
+}
 
