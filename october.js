a14eecf17aa32f87ad19a141fc663a0ce3466709
Артём Дмитренко <darkrogua@gmail.com>
2022-02-25 16:02:44 +0400

    fix

--- october.js
+++ october.js
@@ -1,30 +1,57 @@
+
+const path = require('path');
 var fs = require('fs');
 
 const cssFileList = [
    'pages/index.html',
 ];
 
+let fileContent = [];
+
 cssFileList.forEach((fileName) => {
-   fs.readFile(fileName, function (err, data) {
+   fs.readFile(fileName, function (err, data, fileContent) {
       if (err) throw err;
       var array = data.toString().split("\n");
-     
+      fileContent = array;
       for (i in array) {
          if (array[i].includes('{%')) {
-            console.log(array[i]);
-            getPartialName(array[i]);
+            let promise = new Promise(function (resolve, reject) {
+               fileContent[i] = getPartial(array[i]);
+            });
          } 
       }
+      console.log('test' + fileContent);
    });
+
+
+
+   //fs.appendFile(path.join(__dirname, 'public/pages/index.html'), fileContent, function (err) {
+   //   if (err) throw err;
+   //   console.log('File создан где-то');
+   //});
 });
 
+
 function getPartialName(sLine) {
-   let regexp = '/<(.*?)>/g';
-   let matchAll = sLine.matchAll(regexp);
-   console.log(matchAll);
+   let regexp = /'([^']+)'/gm;
+   let matchAll = regexp.exec(sLine);
+   return matchAll[1];
 }
 
-function getPartial() {
 
+function getPartial(fullString) {
+   let partialName = getPartialName(fullString);
+   if (partialName) {
+    return getInnerPartial(partialName+'.htm');
+   }
+   return 'lox';
 }
 
+function getInnerPartial(partialName) {
+   fs.readFile(path.join(__dirname, partialName), 'utf8', function (err, data) {
+      if (err) {
+         return console.log(err);
+      }
+      return data;
+   });
+}
